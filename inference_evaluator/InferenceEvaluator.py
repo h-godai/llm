7be106f521ae3cc58745fd4cdc211cad47330fdb
h-godai <godai@techarts.co.jp>
2024-12-05 01:25:31 +0900

    Update InferenceEvaluator.py

--- inference_evaluator/InferenceEvaluator.py
+++ inference_evaluator/InferenceEvaluator.py
@@ -1,7 +1,16 @@
+
 # InferenceEvaluatorのclass
 # Elyza-task-100形式の評価データを使って自動評価するクラス
 # Author: h.godai 2024/12/5
-#
+# 2024/12/9 prompt変更
+# 2024/12/10 dpo eval用output記録
+# 2024/12/10 promptを外部から与えられるように修正
+# 2024/12/12 scoremapを追加
+# 2024/12/12 suffixを追加
+# 2024/12/13 command_prompt_を追加
+# 2024/12/13 prefix/suffixをresultに書き出す
+# 2024/12/15 Geminiに対応
+# 2024/12/16 No evalに対応
 # 参考資料
 #
 # https://github.com/openai/openai-cookbook/blob/main/examples/How_to_handle_rate_limits.ipynb
@@ -35,18 +44,28 @@ import pytz    # Import the pytz module
 import time
 import openai
 import pandas as pd
-
+from tqdm import tqdm
 from google.colab import userdata
 from tenacity import (
     retry,
     stop_after_attempt,
     wait_random_exponential,
 )
+import google.generativeai as genai
+
+# 検証済みのモデル
+# OpenAI
+#  gpt-4o-2024-11-20        (default)
+#  gpt-4o-mini-2024-07-18
+# GEMINI
+#  gemini-1.5-pro           (default)
+#  gemini-1.5-flash
 
 
-# 評価クラス定義
+
+# 評価クラス定期
 class InferenceEvaluator:
-  model_ = None
+  model_ = None       # target LLM model
   model_name_ = ""
   tokenizer_ = None
   max_tokens_ = 512
@@ -60,30 +79,68 @@ class InferenceEvaluator:
   pasttime_ = 0
   ai_client = None
   output_result_ = []
+  prefix_prompt_ = ""
+  suffix_prompt_ = ""
+  command_prompt_ = "### 指示\n"
+  input_prompt_hook_ = None # few shot, RAG用の入力時のフック
 
+  use_gemini_ = True
+  openai_api_key_ = None
 
   # 初期化
-  def __init__(self,  model, openapikey = None):
-    self.model_ = model
-    if openapikey is None:
-      openai_api_key = userdata.get('OPENAI_API_KEY')
+  # apikeyが無い場合は、評価なし
+  def __init__(self,  target_model, apikey, eval_model = "gpt"):
+    self.model_ = target_model
+    self.openai_api_key_ = apikey
+    self.suffix_prompt_= ""
+    self.input_prompt_hook_ = None
+    if apikey is None:
+      print("not eval mode")
+      self.eval_ai_model_ = "-"
+      self.ai_client_ = None
+      return
+
+    if eval_model.startswith("gpt"):
+      # OpenAIのgpt系の場合
+      self.use_gemini_ = False
+      self.eval_ai_model_ = eval_model if eval_model != "gpt" else "gpt-4o-2024-11-20"
+      self.ai_client_ = openai.OpenAI(api_key=self.openai_api_key_)
+      print(self.ai_client_)
+      self.eval_ai_model_ =  "gpt-4o-2024-11-20" # "gpt-4o-mini-2024-07-18" #
     else:
-      openai_api_key = openapikey
-    self.ai_client = openai.OpenAI(api_key=openai_api_key)
-    self.eval_ai_model_ = "gpt-4o-2024-11-20"
+      # Google系の場合
+      self.use_gemini_ = True
+      self.eval_ai_model_ = eval_model if eval_model != "gemini" else "gemini-1.5-pro"
+      genai.configure(api_key=self.openai_api_key_)
+      #model = genai.GenerativeModel("gemini-1.5-flash")
+      self.ai_client_ = genai.GenerativeModel("gemini-1.5-pro")
+      self.eval_ai_model_ = "gemini-1.5-pro"
+    print(f"using {self.eval_ai_model_} ai model")
+  pass      
+
 
   # 推論の実行
   def inference(self, prompt):
-    print(f"\n======= TASK-{self.eval_count_} : {prompt}\n")
-    inputtext = f"### 指示\n{prompt}\n### 回答\n"
-    inputs = tokenizer(inputtext, #prompt,
-                      return_tensors="pt",
-                      padding=True,
-                      max_length=1024,        # 最大トークン長を設定（モデルの制限に合わせる）
-                      truncation=True).to("cuda")
-    # テキスト生成
-    # outputs = model.generate(**inputs, max_new_tokens = 512, use_cache = True, do_sample=False, repetition_penalty=1.2)
-    # prediction = tokenizer.decode(outputs[0], skip_special_tokens=True).split('\n### 回答')[-1]
+    print(f"\n======= TASK-{self.eval_count_} :\n")
+    inputtext = None
+    if self.input_prompt_hook_:
+      # プロンプティング用のフック
+      inputtext = self.input_prompt_hook_(self, prompt)
+    if inputtext is None:
+      # デフォルトのプロンプト
+      inputtext = f"{self.prefix_prompt_}{self.command_prompt_}{prompt}\n{self.suffix_prompt_}### 回答\n"
+    print(inputtext)
+
+    inputs = self.tokenizer_(inputtext, 
+                             return_tensors="pt",
+                             padding=True,
+                             max_length=1024,        # 最大トークン長を設定（モデルの制限に合わせる）
+                             truncation=True)
+
+    if next(self.model_.parameters()).is_cuda:
+        inputs = inputs.to("cuda")    # テキスト生成
+
+    # 推論の開始
     output = self.model_.generate(
       inputs["input_ids"],
       max_new_tokens = self.max_tokens_,  # 最大生成トークン数
@@ -93,16 +150,22 @@ class InferenceEvaluator:
       temperature = self.temperature_,    # 出力の多様性を制御
       top_p=self.top_p_                   # トップ確率のフィルタリング
     )
-    # 結果をデコードして表示
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True).split('\n### 回答')[-1]
-    return generated_text
 
-  # 評価用のAPIコール
+    # 結果をデコードして返す
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True).split('\n### 回答')[-1]
+    return generated_text.strip()
+
+  # リトライ付きのOpenAIのAPIコール
   @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
   def completion_with_backoff(self, **kwargs):
-    return self.ai_client.chat.completions.create(**kwargs)
+    return self.ai_client_.chat.completions.create(**kwargs)
 
-  # 評価
+  # リトライ付きのGEMINIのAPIコール
+  @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+  def gemni_completion_with_backoff(self, prompt):
+    return self.ai_client_.generate_content(prompt)
+
+  # 評価実行
   def gpt4eval(self, pred, input_text, output_text, eval_aspect):
     prompt = f"""あなたは採点者です。
 
@@ -136,25 +199,51 @@ class InferenceEvaluator:
 {pred}
 """
 
-    response = self.completion_with_backoff(
-        model=self.eval_ai_model_,
-        messages=[{"role": "user", "content": prompt}],
-        temperature=0,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-    gpt4score = response.choices[0].message.content
+    try:
+      if self.use_gemini_:
+        response = self.gemni_completion_with_backoff(prompt)
+        gpt4score = response.text
+      else:
+        response = self.completion_with_backoff(
+            model=self.eval_ai_model_,
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+        gpt4score = response.choices[0].message.content
+
+    except openai.RateLimitError as e:
+      # Handle rate limit errors gracefully
+      print(f"Rate limit exceeded. Retrying in a moment... Error: {e}")
+      # You might want to implement a more sophisticated retry mechanism here,
+      # such as exponential backoff, to avoid hitting the rate limit repeatedly.
+      time.sleep(60)  # Wait for 60 seconds before retrying
+      # Retry the request
+      if self.use_gemini_:
+        response = self.gemni_completion_with_backoff(prompt)
+        gpt4score = response.text
+      else:
+        response = self.completion_with_backoff(
+            model=self.eval_ai_model_,
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0,
+            frequency_penalty=0,
+            presence_penalty=0,
+        )
+        gpt4score = response.choices[0].message.content
     try:
         gpt4score = int(gpt4score)
     except ValueError:
         print(f"Evaluation failed: {gpt4score}")
         gpt4score = None
-    time.sleep(0.5) # OpenAIのAPIコールに猶予を与えるため
+    time.sleep(0.5) # APIコールに猶予を与えるため
     return gpt4score
 
   # 推論と評価
   def inference_and_eval(self, evaltext):
-
+    if self.ai_client_ is None:
+      return 0 # not eval mode
     start = time.time()  # 現在時刻（処理開始前）を取得
     generated_text = self.inference(evaltext['input']).strip()
     pasttime = (time.time()- start)
@@ -203,10 +292,15 @@ class InferenceEvaluator:
   # detail_pathで指定したフォルダに詳細な情報のファイルをexcelかcsv形式で格納する
   def write_result(self, pathname = "LLM2024_result.csv", detail_path = "./outputs", to_excel = True, to_csv = False):
     now = datetime.datetime.now(pytz.timezone("Asia/Tokyo")).strftime('%Y/%m/%d %H:%M:%S')
-    resultstr=f"{self.model_name_},{self.temperature_},{self.max_tokens_},{self.average_score_},{self.eval_ai_model_},{now},{self.pasttime_:.1f},0\n"
+    scmap = [0,0,0,0,0]
+    for row in self.output_result_:
+      score = row['score']
+      scmap[score-1] += 1
+    print(scmap)
+    resultstr=f"{self.model_name_},{self.temperature_},{self.max_tokens_},{self.average_score_},{self.eval_ai_model_},{now},{self.pasttime_:.1f},0,{scmap},{self.prefix_prompt_},{self.suffix_prompt_}\n"
     print(resultstr)
     if pathname is not None:
-      !touch $pathname
+      #!touch $pathname
       with open(pathname, 'a', encoding='utf-8') as file:
         file.write(resultstr)
         file.close()
@@ -233,7 +327,7 @@ class InferenceEvaluator:
   #   InferenceEvaluator.test(model, tokenizer)
   def test(model, tokenizer):
     FastLanguageModel.for_inference(model)
-    test = InferenceEvaluator(model, userdata.get('OPENAI_API_KEY'))
+    test = InferenceEvaluator(model, OPENAI_API_KEY) #userdata.get('OPENAI_API_KEY'))
     dataset = [{"input": "仕事の熱意を取り戻すためのアイデアを5つ挙げてください。", "output":"努力、根性、気合、情熱、無心", "eval_aspect": "答えが5つあれば5点、それ以外は1点" },
                {"input": "日本で一番低い山は？", "output":"日和山", "eval_aspect": "日和山なら5点、さもなくば1点" }]
     test.run(dataset = dataset,
@@ -241,3 +335,4 @@ class InferenceEvaluator:
               tokenizer = tokenizer,
               max_tokens= 256)
     test.write_result("InferenceEvaluatorTest", ".")
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='firestom',
+    version='0.1.0',
+    description='A simple library for LLM',
+    author='h.godai',
+    author_email='godai@techarts.co.jp',
+    url='https://github.com/h-godai/llm',
+    packages=find_packages(),
+    python_requires='>=3.10',
+)
